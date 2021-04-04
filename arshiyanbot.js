e3a3956aa721a19f16c3a3d70918686aae27295d
Mohammad Javad Arshiyan <dr.bloger@gmail.com>
2021-04-04 08:18:30 +0430

    Update arshiyanbot.js
    update selectors

--- arshiyanbot.js
+++ arshiyanbot.js
@@ -6,8 +6,8 @@ function instagramDeleter() {//  create a deleter loop function
         randtime = Math.floor(Math.random() * 100000); // make a random number
         console.log("next run : "+randtime);
         document.querySelector("#react-root > section > main > div > div._2z6nI > div:nth-child(1) > div > article:nth-child(1) > div.MEAGs > button").click();  //  selector to click on options menu
-        setTimeout(function () { document.querySelector("body > div.RnEpo.Yx5HN > div > div > div > button.aOOlW.-Cab_").click(); }, 2000)         //  selector to click delete option
-        setTimeout(function () { document.querySelector('body > div.RnEpo.Yx5HN > div > div > div.mt3GC > button.aOOlW.-Cab_').click(); }, 4000)         //  selector to click delete confirmation
+        setTimeout(function () { document.querySelector("body > div.RnEpo.Yx5HN > div > div > div > div > button.aOOlW.-Cab_").click(); }, 2000)         //  selector to click delete option
+        setTimeout(function () { document.querySelector('body > div.RnEpo.Yx5HN > div > div > div > div.mt3GC > button.aOOlW.-Cab_').click(); }, 4000)         //  selector to click delete confirmation
         console.log (i+1 +" Deleted")                                                                   //  delete numbering to console
         i++;                                                                                            //  increment the counter
         if (i < 5) {                                                                                    //  if the counter < 5, call the loop function
